e05bf5eeb27fb341c181797cb7a1a4d83ffad9d1
Wybe Westra <wybe@ruurdwestra.nl>
2021-02-07 13:09:08 +0100

    + Add input and output amount change functions.

--- data/logic_node.gd
+++ data/logic_node.gd
@@ -1,11 +1,40 @@
 extends Object
 
+# Array of slots: {"object": <logic_node>, "slot": <int>}
+# If slot not connected: null
+# An input slot can only have one connection going into it.
+var _inputs = []
+# Array of slots and their connections: {<logic_node>: <slot (int)>, <other_node>: <slot (int)>}
+# If slot has no connections: null
+# An output can be connected to multiple inputs.
+var _outputs = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func set_inputs_amount(amount: int):
+	amount = max(amount, 0)
+	
+	# TODO: disconnect from any nodes that were connected
+	#       to any slots we will  remove.
+	
+	_inputs.resize(amount)
+
+
+func set_outputs_amount(amount: int):
+	amount = max(amount, 0)
+	
+	# TODO: disconnect from any nodes that were connected
+	#       to any slots we will  remove.
+	
+	_outputs.resize(amount)
+
+
+func get_inputs_amount() -> int:
+	return _inputs.size()
+
+
+func get_outputs_amount() -> int:
+	return _outputs.size()
--- tests/unit/data/test_logic_node.gd
+++ tests/unit/data/test_logic_node.gd
@@ -7,8 +7,36 @@ var _logic: Object = null
 
 
 func before_each():
-	_logic = LogicNode.new()
+	_logic = autofree(LogicNode.new())
 
 
 func test_new():
 	assert_not_null(_logic)
+
+
+func test_inputs_amount():
+	_logic.set_inputs_amount(1)
+	assert_eq(_logic.get_inputs_amount(), 1)
+	
+	_logic.set_inputs_amount(4)
+	assert_eq(_logic.get_inputs_amount(), 4)
+	
+	_logic.set_inputs_amount(2)
+	assert_eq(_logic.get_inputs_amount(), 2)
+	
+	_logic.set_inputs_amount(-10)
+	assert_eq(_logic.get_inputs_amount(), 0)
+
+
+func test_outputs_amount():
+	_logic.set_outputs_amount(1)
+	assert_eq(_logic.get_outputs_amount(), 1)
+	
+	_logic.set_outputs_amount(4)
+	assert_eq(_logic.get_outputs_amount(), 4)
+	
+	_logic.set_outputs_amount(2)
+	assert_eq(_logic.get_outputs_amount(), 2)
+	
+	_logic.set_outputs_amount(-10)
+	assert_eq(_logic.get_outputs_amount(), 0)
