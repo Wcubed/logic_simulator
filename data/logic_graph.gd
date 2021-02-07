a7144fbd4abfa841bcf38f07ac5ee7e732774117
Wybe Westra <wybe@ruurdwestra.nl>
2021-02-07 16:45:31 +0100

    + Can now connect and disconnect logic nodes.

--- data/logic_graph.gd
+++ data/logic_graph.gd
@@ -27,3 +27,37 @@ func remove_node(id: int):
 	# TODO: remove connections to and from this node.
 	
 	_nodes.erase(id)
+
+
+# Connect the output slot of the first node, to the input slot of the second node.
+# Does nothing if either a node does not exist, or one of the slots does not exist.
+func connect_nodes(first_id: int, output_slot: int, second_id: int, input_slot):
+	var first_node: LogicNode = _nodes.get(first_id)
+	var second_node: LogicNode = _nodes.get(second_id)
+	
+	if first_node == null or second_node == null:
+		return
+	if first_node.get_outputs_amount() <= output_slot and second_node.get_inputs_amount() <= input_slot:
+		return
+	
+	first_node.connect_output(output_slot, second_id, input_slot)
+	second_node.connect_input(input_slot, first_id, output_slot)
+
+
+func disconnect_nodes(first_id: int, output_slot: int, second_id: int, input_slot):
+	var first_node: LogicNode = _nodes.get(first_id)
+	var second_node: LogicNode = _nodes.get(second_id)
+	
+	if first_node == null or second_node == null:
+		return
+	
+	var input_connection = second_node.get_inputs()[input_slot]
+	if input_connection["id"] != first_id or input_connection["slot"] != output_slot:
+		# The connection does not exist.
+		# We do not need to check the output node for a connection, because
+		# that can have multiple, and won't do anything when we disconnect a
+		# nonexistent connection.
+		return
+	
+	first_node.disconnect_output(output_slot, second_id, input_slot)
+	second_node.disconnect_input(input_slot)
---
+++ tests/integration/data/test_logic_graph.gd
@@ -0,0 +1,35 @@
+extends "res://addons/gut/test.gd"
+
+var InputNode := load("res://data/logic_nodes/logic_input.gd")
+var And := load("res://data/logic_nodes/logic_and.gd")
+var Not := load("res://data/logic_nodes/logic_not.gd")
+
+var _graph: Object = null
+
+
+func before_each():
+	_graph = autofree(load("res://data/logic_graph.gd").new())
+
+
+func test_connect_and_disconnect_nodes():
+	var and_node: LogicNode = And.new()
+	var not_node: LogicNode = Not.new()
+	
+	var and_id: int = _graph.add_node(and_node)
+	var not_id: int = _graph.add_node(not_node)
+	
+	_graph.connect_nodes(and_id, 0, not_id, 0)
+	
+	var and_outputs = and_node.get_outputs()
+	var not_inputs = not_node.get_inputs()
+	
+	assert_eq(and_outputs[0][0]["id"], not_id,  "test")
+	assert_eq(and_outputs[0][0]["slot"], 0)
+	
+	assert_eq(not_inputs[0]["id"], and_id)
+	assert_eq(not_inputs[0]["slot"], 0)
+	
+	_graph.disconnect_nodes(and_id, 0, not_id, 0)
+	
+	assert_eq(and_outputs[0].size(), 0)
+	assert_eq(not_inputs[0], null)
